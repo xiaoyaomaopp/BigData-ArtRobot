269395a94a907de5fbc1b04e6524a4e19d254a95
yrr626 <437548960@qq.com>
2017-08-17 09:47:28 +0800

    页面数据爬取

--- c_bbc/e_fbDemo.js
+++ c_bbc/e_fbDemo.js
@@ -6,8 +6,8 @@ var extractor = {
     //表名
     name: 'bbc.histroy',//板块
     //  网页是否由它处理的依据，如匹配区域网址 http[s]://shanghai.anjuke.com/market[/]...
-    target: /^http:\/\/www\.bbc\.co\.uk\/ahistoryoftheworld\/objects\/CkcC4hEsTa\-alS5gtLG3vg/,
-    //target: /^http:\/\/www\.bbc\.co\.uk\/ahistoryoftheworld\/objects/g,
+    //target: /^http:\/\/www\.bbc\.co\.uk\/ahistoryoftheworld\/objects\/tbi5Dw6gTHepMf7jSNjn_g/,
+    target: /^http:\/\/www\.bbc\.co\.uk\/ahistoryoftheworld\/objects/g,
     //target: /^http:\/\/shanghai\.anjuke\.com\/market\/$/i,
     //正则，加入爬取队列，只为寻找target
     //helpUrl:/^http:\/\/www\.datayuan\.cn/g,
@@ -34,31 +34,40 @@ var extractor = {
     times: 0,
     //无返回值框架不会保存，可使用new this.model(data)自行操作
     handler: function ($, queueItem, responseBuffer, response) {
+        console.log($)
         var idArr =queueItem.url.split("/");
         var id = idArr[idArr.length-1];
-        console.log('id'+id)
-
         var title = $("#blq-content h1").html();
         if(!!title) title = title.replace('\n','');
-        console.log('title'+title);
-        var location = $($("#blq-content .row #explore .shadowed-box-mid dl").children()[1]).find("span").html();
-        console.log('location'+location)
-        var description = $("#blq-content .content .object-description").html()
-        console.log('description'+description)
-        console.log($($("#blq-content .row #explore .shadowed-box-mid dl").children()[1]).find("span").html());
-        console.log($($("#blq-content .row #explore .shadowed-box-mid dl").children()[1]).find("span").html());
-        console.log($($($("#blq-content .row #explore .shadowed-box-mid dl").children()[1]).find("a").children()[1]).find("span").html());
-        console.log($($("#blq-content .row #explore .shadowed-box-mid dl").children()[2]).find("span").html());
-        console.log($($("#blq-content .row #explore .shadowed-box-mid dl").children()[3]).find("span").html());
-        console.log($($("#blq-content .row #explore .shadowed-box-mid dl").children()[4]).find("span").html());
-        console.log($($("#blq-content .row #explore .shadowed-box-mid dl").children()[5]).find("span").html());
-        console.log($($("#blq-content .row #explore .shadowed-box-mid dl").children()[6]).find("span").html());
-        console.log($($("#blq-content .row #explore .shadowed-box-mid dl").children()[7]).find("span").html());
-        console.log($($("#blq-content .row #explore .shadowed-box-mid dl").children()[8]).find("span").html());
-        console.log($($("#blq-content .row #explore .shadowed-box-mid dl").children()[9]).find("span").html());
-        console.log($($("#blq-content .row #explore .shadowed-box-mid dl").children()[10]).find("span").html());
-        console.log($($("#blq-content .row #explore .shadowed-box-mid dl").children()[11]).find("span").html());
-        console.log($($("#blq-content .row #explore .shadowed-box-mid dl").children()[12]).find("span").html());
+        var description = $("#blq-content .content .object-description").html();
+        var detail = {
+            id : id,
+            title : title,
+            description : description
+        };
+        $("#blq-content .row #explore .shadowed-box-mid dd").each(function(){
+            var key = $(this).prev().html().trim();
+            var value = '';
+            $(this).find('ul li span').each(function(i){
+                if(!!$(this).html()){
+                    if(0!=i) value += '/';
+                    value += $(this).html().replace('\n','').trim();
+                }
+            });
+            if('Size'==key){
+                value += '/';
+                $(this).find('.small').children().each(function(i){
+                    if((i+1)%2==0) value += ' ';
+                    value += $(this).html().replace('\n','').trim();
+                });
+            }else{
+                if(!!$(this).find('.small').html()) value += '/'+$(this).find('.small').html().replace('\n','').trim();
+            }
+            if('W:'!=key && 'D:'!=key && 'H:'!=key){
+                detail[key] = value;
+            }
+        });
+        console.log(detail);
     }
 }
 
